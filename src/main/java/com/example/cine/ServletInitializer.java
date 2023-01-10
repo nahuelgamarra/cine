131b986675d17c700c04e4e5994fcf8d18eeabc5
Nahuel Gamarra <gamarraorlandonahuel@gmail.com>
2023-01-09 23:19:20 -0300

    primer commit

---
+++ src/main/java/com/example/cine/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.cine;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CineApplication.class);
+	}
+
+}
