4659d5bc1a60c57b8dee927b712d60eda13cbd6a
Nahuel Gamarra <gamarraorlandonahuel@gmail.com>
2023-01-09 23:34:38 -0300

    segundo commit

---
+++ src/main/java/com/example/cine/models/Usuario.java
@@ -0,0 +1,40 @@
+package com.example.cine.models;
+
+
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long idUser;
+    private String name;
+    private String email;
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
